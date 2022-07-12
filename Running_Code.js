b65b91c5d8f664af7c9cf1470f745637d33e7c74
unknown <hariono26011996@gmail.com>
2022-07-12 15:24:46 +0700

    fundamentalday1

---
+++ Running_Code.js
@@ -0,0 +1,22 @@
+// comment itu adalag tulisan di code yang tidak akan diexecute
+// shortcut utk comment adalah ctrl + "/"
+// comment berfungsi sebagai berikut:
+// 1. catatan  per code
+// ex: ini
+// ini logaritma untuk page login
+
+// 2. catatan komunikasi pribadi atau antar team
+
+// comment untuk satu line bisa pakai double slash / "//"
+// example:
+// ini comment
+// ini comment 2
+
+// comment untuk lebih dari satu line bisa pakai "/**/"
+// example :
+/* ini
+untuk comment
+lebih dari 1 line*/
+
+
+console.log('Hello World')
